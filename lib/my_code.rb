53a5898a4d7f0959fc198af8a75653a1c363f52c
patrick leal <pjleal3846@icloud.com>
2020-01-24 02:24:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,4 @@
 def map_to_negativize(source_array)
-map_to_no_change(source_array)
-map_to_double(source_array)
+def map_to_no_change(source_array)
+def map_to_double(source_array)
 map_to_square(source_array)# My Code here....
